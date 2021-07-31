cc58ee0528b8a3ca9b56f1f0dd01170185731bc8
Sean Ammirati <ammirati.sean@gmail.com>
2021-07-31 18:33:27 -0400

    Tutorial: Up to routing

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,9 +1,30 @@
 import 'package:angular/angular.dart';
 
+import 'src/hero.dart';
+import 'src/hero_component.dart';
+
+import 'src/hero_service.dart';
+
 @Component(
   selector: 'my-app',
-  template: '<h1>Hello {{name}}</h1>',
+  templateUrl: 'app_component.html',
+  styleUrls: ['app_component.css'],
+  directives: [coreDirectives, HeroComponent],
+  providers: [ClassProvider(HeroService)],
 )
-class AppComponent {
-  var name = 'Angular';
+class AppComponent implements OnInit {
+  final title = 'Tour of Heroes';
+  List<Hero> heroes;
+
+  Future<void> _getHeroes() async {
+    heroes = await _heroService.getAll();
+  }
+
+  void ngOnInit() => _getHeroes();
+  Hero selected;
+
+  void onSelect(Hero hero) => selected = hero;
+
+  final HeroService _heroService;
+  AppComponent(this._heroService);
 }
